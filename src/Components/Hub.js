9db57bf5a0ca9d15c6280739e4eb0258c1d8a471
Kyle Ortiz <kyleaortiz@gmail.com>
2021-08-23 17:21:25 -0500

    added working pomo timer, need to figure out how to start and stop timer

--- src/Components/Hub.js
+++ src/Components/Hub.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ToDoList from './ToDoList';
 import SignOut from './SignOut';
+import Pomodoro from './Pomodoro';
 
 function Hub(props) {
      const auth = props.auth;
      return (
           <div className="dashboard">
+               <div className="tiles">
                <ToDoList />
+               <Pomodoro />
+               </div>
                <SignOut auth={auth}/>
           </div>
      )
--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,9 +1,39 @@
 import React,{useState,useEffect} from 'react'
 
 function Pomodoro() {
+     const [minutes,setMinutes] = useState(25);
+     const [seconds,setSeconds] = useState(10);
+     const [message,setMessage] = useState(false);
+
+     const displayedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+     const displayedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+     useEffect(() => {
+          let interval = setInterval(()=> {
+               clearInterval(interval);
+
+               if(seconds === 0 && minutes !== 0) {
+                    setSeconds(59)
+                    setMinutes(minutes - 1)
+               } else if (seconds === 0 && minutes === 0) {
+                    const newTimerMinutes = message ? 24 : 4;
+                    const newTimerSeconds = 59
+
+                    setMinutes(newTimerMinutes);
+                    setSeconds(newTimerSeconds);
+                    setMessage(!message);
+               } else {
+                    setSeconds(seconds - 1 )
+               }
+
+          },1000)
+     }, [seconds])
+
      return (
-          <div>
-               
+          <div className="Pomo-timer">
+               {message ? <h2>Break Time! New Session Starts in : </h2> : <h2>Keep Working. You got this!</h2>}
+               <div className="clock">
+                    {displayedMinutes}:{displayedSeconds}
+               </div>
           </div>
      )
 }
